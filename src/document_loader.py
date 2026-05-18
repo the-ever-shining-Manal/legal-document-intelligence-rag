aeba22d18cbb226effea5e50a7aefe2dd35c4f4e
moonal2005 <f5skmdtzgv@privaterelay.appleid.com>
2026-05-18 22:02:25 +0300

    working...

---
+++ src/document_loader.py
@@ -0,0 +1,7 @@
+import fitz
+
+doc=r"C:\Users\SOFT LAPTOP\OneDrive - Faculty Of Engineering (Tanta University)\Desktop\self-study\courses\legal-document-intelligence-rag\Legal_Document\integralversionprinciples2010-e.pdf"
+
+
+def load_document(document):
+    
